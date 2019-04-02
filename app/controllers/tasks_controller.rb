e2145dd2bb6e799e8ee8ad74d9323afca7ed8e18
sacrament <xkinked.tailx@gmail.com>
2019-04-02 23:35:47 +0900

    tasks_conrollerのindex内の処理が肥大化したためprivateにまとめてメソッド化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,9 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i(show edit update destroy)
   before_action :not_enter_no_Sign_in_user, only: %i(index new show edit)
+  
   def index
-    
-    title = params[:search_title] if params[:search_title].present?
-    status = params[:search_status] if params[:search_status].present?
-
-    if params[:search_flag] == "true"     # 検索フォームからの処理か分岐
-      if params[:search_title].present? && params[:search_status].present?    # 入力欄どちらもあり
-        @tasks = current_user.tasks.page(params[:page]).status_title_like_where(status, title)
-      elsif params[:search_title].empty? && params[:search_status].present?     # タイトルのみ空
-        @tasks = current_user.tasks.page(params[:page]).status_like_where(status)
-      elsif params[:search_title].present? && params[:search_status].empty?      # ステータスのみ空
-        @tasks = current_user.tasks.page(params[:page]).title_like_where(title)
-      elsif params[:search_title].empty? && params[:search_status].empty?  # ステータスもタイトルも空
-        @tasks = current_user.tasks.page(params[:page]).created_at_desc
-      end
-    else     # 検索フォームから以外の処理
-      if params[:sort_flag] == "deadline"       #ソートリンク(終了期限でソート)からの処理
-        @tasks = current_user.tasks.page(params[:page]).deadline_asc
-      elsif params[:sort_flag] == "priority"    #ソートリンク(優先順位でソート)からの処理
-        @tasks = current_user.tasks.page(params[:page]).priority_asc
-      else
-        @tasks = current_user.tasks.page(params[:page]).created_at_desc           #ソートリンク(タスク追加が新しい順にソート)からの処理
-      end
-    end
+    search_diverge
   end
 
   def new
@@ -75,5 +54,30 @@ class TasksController < ApplicationController
   
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def search_diverge
+    title = params[:search_title] if params[:search_title].present?
+    status = params[:search_status] if params[:search_status].present?
+
+    if params[:search_flag] == "true"     # 検索フォームからの処理か分岐
+      if params[:search_title].present? && params[:search_status].present?    # 入力欄どちらもあり
+        @tasks = current_user.tasks.page(params[:page]).status_title_like_where(status, title)
+      elsif params[:search_title].empty? && params[:search_status].present?     # タイトルのみ空
+        @tasks = current_user.tasks.page(params[:page]).status_like_where(status)
+      elsif params[:search_title].present? && params[:search_status].empty?      # ステータスのみ空
+        @tasks = current_user.tasks.page(params[:page]).title_like_where(title)
+      elsif params[:search_title].empty? && params[:search_status].empty?  # ステータスもタイトルも空
+        @tasks = current_user.tasks.page(params[:page]).created_at_desc
+      end
+    else     # 検索フォームから以外の処理
+      if params[:sort_flag] == "deadline"       #ソートリンク(終了期限でソート)からの処理
+        @tasks = current_user.tasks.page(params[:page]).deadline_asc
+      elsif params[:sort_flag] == "priority"    #ソートリンク(優先順位でソート)からの処理
+        @tasks = current_user.tasks.page(params[:page]).priority_asc
+      else
+        @tasks = current_user.tasks.page(params[:page]).created_at_desc           #ソートリンク(タスク追加が新しい順にソート)からの処理
+      end
+    end
   end
 end
