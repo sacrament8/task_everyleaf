a7f2b92bc30eddac4b2773d8f67cc5442cf8985a
sacrament <xkinked.tailx@gmail.com>
2019-04-03 12:08:56 +0900

    管理ユーザー用ユーザー一覧にもページネーションを実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: %i(show edit update destroy)
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page])
   end
 
   def new
